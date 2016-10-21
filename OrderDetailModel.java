69443b94b71220f5465f8875aff97eeaebbf03a9
leizgl <leizopen123@163.com>
2016-10-21 11:38:04 +0800

    20161021 1138

---
+++ OrderDetailModel.java
@@ -0,0 +1,27 @@
+package com.wechat.shop.model.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import com.wechat.shop.model.enums.UserGroup;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "ORDER_DETAIL")
+@GenericGenerator(name = "wechat-shop-id", strategy = "uuid"){
+	
+	
+}  
